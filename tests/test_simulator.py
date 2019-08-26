0d267c45545ee5e4adf9902cbae5bebbff41663d
Robin Henry <robin.henry012@gmail.com>
2019-08-26 14:19:48 +0200

    added simulator pfe tests

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -5,8 +5,13 @@ import numpy as np
 from gym_smartgrid.simulator import Simulator
 
 
-class TestSimulatorBus2OnlyLoad(unittest.TestCase):
+class TestSimulator(unittest.TestCase):
+
     def setUp(self):
+        self.d = 5
+
+    def test_pfe_2bus_only_load(self):
+        """ A distribution network with only 2 buses: Gen --- Load. """
         case = {'baseMVA': 100}
         case['bus'] = np.array([
             [0, 3, 0, 0, 10, 1, 1],
@@ -20,20 +25,17 @@ class TestSimulatorBus2OnlyLoad(unittest.TestCase):
             [0, 1, 1, 1, 0, 100, 0, 0, 1, -360, 360]
         ])
 
-        self.simulator = Simulator(case)
+        simulator = Simulator(case)
+        state, _, _, _ = simulator.transition([-10], [], [], [], [])
 
-    def test_pfe(self):
-        state, _, _, _ = self.simulator.transition([-10], [], [], [], [])
+        npt.assert_almost_equal(state['P_DEV'], [13.81966011, -10.], decimal=self.d)
+        npt.assert_almost_equal(state['Q_DEV'], [13.81966011, -10.], decimal=self.d)
+        npt.assert_almost_equal(state['P_BUS'], [13.81966011, -10.], decimal=self.d)
+        npt.assert_almost_equal(state['Q_BUS'], [13.81966011, -10.], decimal=self.d)
+        npt.assert_almost_equal(state['V_BUS'], [1 + 0.j, 0.7236067 + 0.j], decimal=self.d)
+        npt.assert_almost_equal(state['P_BR'], [13.81966011], decimal=self.d)
 
-        npt.assert_almost_equal(state['P_DEV'], [13.81966011, -10.])
-        npt.assert_almost_equal(state['Q_DEV'], [13.81966011, -10.])
-        npt.assert_almost_equal(state['P_BUS'], [13.81966011, -10.])
-        npt.assert_almost_equal(state['Q_BUS'], [13.81966011, -10.])
-        npt.assert_almost_equal(state['V_BUS'], [1 + 0.j, 0.7236067 + 0.j])
-        npt.assert_almost_equal(state['P_BR'], [13.81966011])
-
-class TestSimulatorBus3OnlyLoad(unittest.TestCase):
-    def setUp(self):
+    def test_pfe_3bus_only_load(self):
         case = {'baseMVA': 100}
         case['bus'] = np.array([
             [0, 3, 0, 0, 10, 1, 1],
@@ -50,24 +52,19 @@ class TestSimulatorBus3OnlyLoad(unittest.TestCase):
             [0, 2, .5, .25, 0, 100, 0, 0, 1, -360, 360]
         ])
 
-        self.simulator = Simulator(case)
+        simulator = Simulator(case)
+        state, _, _, _ = simulator.transition([-10, -5], [], [], [], [])
 
-    def test_pfe(self):
-        d = 5
-        state, _, _, _ = self.simulator.transition([-10, -5], [], [], [], [])
-
-        npt.assert_almost_equal(state['P_DEV'], [19.09042574, -10., -5.], decimal=d)
-        npt.assert_almost_equal(state['Q_DEV'], [18.95504293, -10., -5.], decimal=d)
-        npt.assert_almost_equal(state['P_BUS'], [19.09042574, -10., -5.], decimal=d)
-        npt.assert_almost_equal(state['Q_BUS'], [18.95504293, -10., -5.], decimal=d)
-        npt.assert_almost_equal(state['P_BR'],  [13.81966011, 5.27076563], decimal=d)
+        npt.assert_almost_equal(state['P_DEV'], [19.09042574, -10., -5.], decimal=self.d)
+        npt.assert_almost_equal(state['Q_DEV'], [18.95504293, -10., -5.], decimal=self.d)
+        npt.assert_almost_equal(state['P_BUS'], [19.09042574, -10., -5.], decimal=self.d)
+        npt.assert_almost_equal(state['Q_BUS'], [18.95504293, -10., -5.], decimal=self.d)
+        npt.assert_almost_equal(state['P_BR'],  [13.81966011, 5.27076563], decimal=self.d)
         npt.assert_almost_equal(state['V_BUS'], [1 * np.exp(0.j),
                                                  0.72360679 * np.exp(1.j * 4.4726e-18),
-                                                 0.96088902 * np.exp(1.j * 0.0130)], decimal=d)
+                                                 0.96088902 * np.exp(1.j * 0.0130)], decimal=self.d)
 
-
-class TestSimulatorBus3LoadVRE(unittest.TestCase):
-    def setUp(self):
+    def test_pfe_3bus(self):
         case = {'baseMVA': 100}
         case['bus'] = np.array([
             [0, 3, 0, 0, 10, 1, 1],
@@ -84,10 +81,8 @@ class TestSimulatorBus3LoadVRE(unittest.TestCase):
             [0, 2, .5, .25, 0, 100, 0, 0, 1, -360, 360]
         ])
 
-        self.simulator = Simulator(case)
+        simulator = Simulator(case)
+        # state, _, _, _ = simulator.transition([], [], [], [], [])
 
-    def test_pfe(self):
-        d = 5
-        state, _, _, _ = self.simulator.transition([], [], [], [], [])
 
 
