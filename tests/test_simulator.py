837240e436b6d5e13fcfa2553e93f2f8c3ddc6d4
Robin Henry <robin.henry012@gmail.com>
2019-08-26 13:29:13 +0200

    added docs/ folder

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,56 +1,93 @@
 import unittest
 import numpy.testing as npt
+import numpy as np
+
+from gym_smartgrid.simulator import Simulator
 
 
-class TestSimulator(unittest.TestCase):
+class TestSimulatorBus2OnlyLoad(unittest.TestCase):
     def setUp(self):
-        pass
+        case = {'baseMVA': 100}
+        case['bus'] = np.array([
+            [0, 3, 0, 0, 10, 1, 1],
+            [1, 1, 0, 0, 10, 1.1, 0.9]
+        ])
+        case['device'] = np.array([
+            [0,  0, 0, 30,   -30, 1, 40,    0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, -1, 1, 100, -100, 1,  1, -100, 0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        case['branch'] = np.array([
+            [0, 1, 1, 1, 0, 100, 0, 0, 1, -360, 360]
+        ])
 
-    def test_check_casefile(self):
-        raise NotImplementedError
+        self.simulator = Simulator(case)
 
-    def test_init(self):
-        raise NotImplementedError
+    def test_pfe(self):
+        state, _, _, _ = self.simulator.transition([-10], [], [], [], [])
 
-    def test_load_case(self):
-        raise NotImplementedError
+        npt.assert_almost_equal(state['P_DEV'], [13.81966011, -10.])
+        npt.assert_almost_equal(state['Q_DEV'], [13.81966011, -10.])
+        npt.assert_almost_equal(state['P_BUS'], [13.81966011, -10.])
+        npt.assert_almost_equal(state['Q_BUS'], [13.81966011, -10.])
+        npt.assert_almost_equal(state['V_BUS'], [1 + 0.j, 0.7236067 + 0.j])
+        npt.assert_almost_equal(state['P_BR'], [13.81966011])
 
-    def test_admittance_matrix(self):
-        raise NotImplementedError
+class TestSimulatorBus3OnlyLoad(unittest.TestCase):
+    def setUp(self):
+        case = {'baseMVA': 100}
+        case['bus'] = np.array([
+            [0, 3, 0, 0, 10, 1, 1],
+            [1, 1, 0, 0, 10, 1.1, 0.9],
+            [2, 1, 0, 0, 10, 1.1, 0.9]
+        ])
+        case['device'] = np.array([
+            [0, 0, 0, 50, -50, 1, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, -1, 1, 100, -100, 1, 1, -100, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, -1, 1, 100, -100, 1, 1, -100, 0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        case['branch'] = np.array([
+            [0, 1,  1,   1, 0, 100, 0, 0, 1, -360, 360],
+            [0, 2, .5, .25, 0, 100, 0, 0, 1, -360, 360]
+        ])
 
-    def test_compute_bus_bounds(self):
-        raise NotImplementedError
+        self.simulator = Simulator(case)
 
-    def test_reset(self):
-        raise NotImplementedError
+    def test_pfe(self):
+        d = 5
+        state, _, _, _ = self.simulator.transition([-10, -5], [], [], [], [])
 
-    def test_get_action_space(self):
-        raise NotImplementedError
+        npt.assert_almost_equal(state['P_DEV'], [19.09042574, -10., -5.], decimal=d)
+        npt.assert_almost_equal(state['Q_DEV'], [18.95504293, -10., -5.], decimal=d)
+        npt.assert_almost_equal(state['P_BUS'], [19.09042574, -10., -5.], decimal=d)
+        npt.assert_almost_equal(state['Q_BUS'], [18.95504293, -10., -5.], decimal=d)
+        npt.assert_almost_equal(state['P_BR'],  [13.81966011, 5.27076563], decimal=d)
+        npt.assert_almost_equal(state['V_BUS'], [1 * np.exp(0.j),
+                                                 0.72360679 * np.exp(1.j * 4.4726e-18),
+                                                 0.96088902 * np.exp(1.j * 0.0130)], decimal=d)
 
-    def test_transition(self):
-        raise NotImplementedError
 
-    def test_manage_storage(self):
-        raise NotImplementedError
+class TestSimulatorBus3LoadVRE(unittest.TestCase):
+    def setUp(self):
+        case = {'baseMVA': 100}
+        case['bus'] = np.array([
+            [0, 3, 0, 0, 10, 1, 1],
+            [1, 1, 0, 0, 10, 1.1, 0.9],
+            [2, 1, 0, 0, 10, 1.1, 0.9]
+        ])
+        case['device'] = np.array([
+            [0, 0, 0, 50, -50, 1, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, -1, 1, 100, -100, 1, 1, -100, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, -1, 1, 100, -100, 1, 1, -100, 0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        case['branch'] = np.array([
+            [0, 1,  1,   1, 0, 100, 0, 0, 1, -360, 360],
+            [0, 2, .5, .25, 0, 100, 0, 0, 1, -360, 360]
+        ])
 
-    def test_get_bus_total_injections(self):
-        raise NotImplementedError
+        self.simulator = Simulator(case)
 
-    def test_solve_pfe(self):
-        raise NotImplementedError
+    def test_pfe(self):
+        d = 5
+        state, _, _, _ = self.simulator.transition([], [], [], [], [])
 
-    def test_get_branch_current(self):
-        raise NotImplementedError
-
-    def test_current_from_voltage(self):
-        raise NotImplementedError
-
-    def test_compute_branch_pq(self):
-        raise NotImplementedError
-
-    def test_get_reward(self):
-        raise NotImplementedError
-
-    def test_get_penalty(self):
-        raise NotImplementedError
 
